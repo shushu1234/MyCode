92eb55be0f1ea88e8554ff82d48991370900d1cd
shushu1234 <1395752337@qq.com>
2018-03-18 18:09:54 +0800

    Modified Singleton

--- 03 设计模式/01 单例模式/05 饿汉模式/Singleton2.java
+++ 03 设计模式/01 单例模式/05 饿汉模式/Singleton2.java
@@ -7,7 +7,8 @@ package com.liuyao.mode;
  */
 
 /**
- * 饿汉模式,好处是线程安全，使用时没有延迟，坏处是可能造成资源浪费。
+ * 饿汉模式,好处是得益于类的加载机制天生线程安全，使用时没有延迟，坏处是可能造成资源浪费。
+ * 单线程下，饿汉和包含性能差别不大，在多线程下，由于饱汉需要加锁和解锁，饿汉效果反而更好
  */
 public class Singleton2 {
     public static final Singleton2 singleton2=new Singleton2();
